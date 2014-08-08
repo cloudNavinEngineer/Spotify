061aa60fb6b4fede51068edf746159a1c9c57e9c
vivek06 <navinchaganti@gmail.com>
2014-08-08 16:00:49 -0700

    fourth commit

--- newtest.py
+++ newtest.py
@@ -10,50 +10,72 @@ def main():
 if __name__ == '__main__':
     main()
 
-pseudo code
--------------------------------
+
 # all functions
 # Function to find center of the list or string
 ptr= findcenter()
 
 # Function to search the track
 tracksearch()
+{
+	if found:
+	resizepoem()
+}
 
 #function to resize right
 resize_right()
+{
+	tracksearch()
+}
+
 
 #function to resize left
 resize_left()
+{
+    tracksearch()
+}
 
+# resize the poem when we find the title track
+resizepoem()
 ------------------------------
 
 Title_left =list.join(0 - center)
 Title_right= list.join(center - end)
 
-tracksearch(Title_left)
-tracksearch(Title_right)
-
-if found
-done:
-
-else
+algo_recurcession()
 {
-resize_right(Title_left)
-resize_left(Title_right)
 
-	if both found done:
+    tracksearch(Title_left)
+    tracksearch(Title_right)
+    
+    if found
+    done:
+    
+    else
+    {
+    resize_right(Title_left)
+    resize_left(Title_right)
+    
+    	if both found done:
+    
+    	# now take the left part and find the track for that 1st
+    	# this way we are splitting the string into 3 but not into equal parts 
+    	# the left is divided into 2 parts  and the 3rd part is right part
+    		else
+    		    split=findcenter(Title_right):
+    			algo_recurcession();
+				split=findcenter(Title_Left):
+				algo_recurcession();
+    			if found done:
+    				else
+    				{
+    					resize_right()
+    				 	resize_left()
+    				}
+    		
+    }
 
-	# now take the left part and find the track for that 1st
-	# this way we are splitting the string into 3 but not into equal parts 
-	# the left is divided into 2 parts  and the 3rd part is right part
-		else
-		    split=findcenter(Title_right):
-			tracksearch(0-split)
-                        tracksearch(split-end)
-			if found done:
-				else
-				{
-					resize_right()
-					resize_left()
-				}
 }
+			
+
+
